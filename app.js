f65adecc550be0bc1f82c4f8dc93e4ed5a1d1064
ayorinne123 <ayorise@outlook.com>
2022-07-19 14:04:52 -0400

    second phase

---
+++ app.js
@@ -0,0 +1,17 @@
+// Elements
+function bold()
+{
+    document.getElementById(text-area1).style.fontWeight = 'bold'
+}
+function italic()
+{
+    document.getElementById(text-area1).style.fontWeight = 'italic'
+}
+function underline()
+{
+    document.getElementById(text-area1).style.fontweight = 'underline'
+}
+function undeline()
+{
+    document.getElementById(text-area1).style.fontweight = 'insert'
+}
